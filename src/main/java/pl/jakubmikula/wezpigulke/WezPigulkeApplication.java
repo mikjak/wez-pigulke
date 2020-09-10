7218d2c474ee32649525b6b6755642ba27be7f58
Jakub Mikuła <jakub.mikula@lhsystems.com>
2020-09-10 13:33:06 +0200

    Initial commit

---
+++ src/main/java/pl/jakubmikula/wezpigulke/WezPigulkeApplication.java
@@ -0,0 +1,13 @@
+package pl.jakubmikula.wezpigulke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WezPigulkeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WezPigulkeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/jakubmikula/wezpigulke/WezPigulkeApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jakubmikula.wezpigulke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WezPigulkeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
